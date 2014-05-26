913ab20a1f0f930a6b7a7acdd81f952c402be063
Angel <ese.acg@gmail.com>
2014-05-26 11:03:42 +0200

    Añadida variable static para pasarle el valor estatico de los números que queremos imprimir

--- Test.java
+++ Test.java
@@ -1,28 +1,28 @@
 
 /**
- * Write a description of class Test here.
+ * Esta clase nos imprime por pantalla los numeros enteros que le pasemos en la variable static y también el tiempo en MINUTOS:SEGUNDOS utilizados para ello
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class Test
 {
-    
+    private static final int NUMBERS = 1000000; 
     /**
      * Constructor for objects of class Test
      */
     public Test()
     {
-        
+       
     }
 
     /**
-     * 
+     * Calcula el tiempo en imprimir valores enteros, imorime el resultado por pantalla
      */
     public void tiempoTranscurrido()
     {
         long initTime = System.currentTimeMillis();
-        for(int i = 0; i < 1000000; i++){
+        for(int i = 0; i < NUMBERS; i++){
             System.out.print(i + " ");            
         }
         long finalTime = System.currentTimeMillis();
